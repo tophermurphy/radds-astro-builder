7598c26f4bd6aea724cf9b7452b15838f8e7ba5c
tophermurphy <tophermurphy@gmail.com>
2023-11-13 11:41:31 -0600

    getting somewhere

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,30 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
+                    @import "@bootstrap/scss/_functions.scss";
+                    @import "src/styles/variables.scss";
+                    @import "@bootstrap/scss/_variables.scss"; 
+                    @import "@bootstrap/scss/_variables-dark.scss"; 
+                    @import "@bootstrap/scss/_maps.scss";
+                    @import "@bootstrap/scss/_mixins.scss";
+                    @import "@bootstrap/scss/root";
+                    @import "@bootstrap/scss/utilities";
+                    @import "@bootstrap/scss/reboot";
+                    @import "@bootstrap/scss/type";
+                    @import "@bootstrap/scss/images";
+                    @import "@bootstrap/scss/containers";
+                    @import "@bootstrap/scss/grid";
+                    @import "@bootstrap/scss/helpers";
+                    @import "@bootstrap/scss/utilities/api";
+                    `,
+                }
+            }
+        }
+    }
+});
---
+++ src/lib/payloadAPI.ts
@@ -0,0 +1,39 @@
+export const getPageList = async () => {
+    const res = await fetch(`${import.meta.env.PAYLOAD_URL}/api/pages/list`);
+    if(!res.ok){
+        throw new Error("Failed to fetch page list.");
+    }
+    return res.json()
+}
+
+export const getPageBySlug = async (slug: string) => {
+    const res = await fetch(`${import.meta.env.PAYLOAD_URL}/api/pages?depth=1&where[slug][equals]=${slug}`);
+    if(!res.ok){
+        throw new Error("Failed to fetch page data.");
+    }
+    const data = await res.json();
+    
+    if( !Array.isArray(data.docs) && data.docs.length < 1 ){
+        throw new Error("No Page is Found");
+    }
+    return data.docs[0];
+}
+
+export const getAPIColors = async () => {
+    const res = await fetch(`${import.meta.env.PAYLOAD_URL}/api/theme_colors`);
+    if(!res.ok){
+        throw new Error("Failed to fetch page data.");
+    }
+    const data = await res.json();
+
+    return data;
+}   
+
+export const getPayloadSiteOptions = async () => {
+    const res = await fetch(`${import.meta.env.PAYLOAD_URL}/api/globals/site_options`);
+    if(!res.ok){
+        throw new Error("Failed to fetch site options.");
+    }
+    return res.json()
+}
+
