8c6d651642873a1198ea187f2e63ef3040904ee4
tophermurphy <tophermurphy@gmail.com>
2023-11-29 16:19:33 -0800

    carousel good start

--- src/types/payload-types.d.ts
+++ src/types/payload-types.d.ts
@@ -6,13 +6,17 @@
  * and re-run `payload generate:types` to regenerate this file.
  */
 
-export type ListPart = {
-  title: string;
-  textEditor?: {
-    [k: string]: unknown;
-  }[];
-  id?: string;
-}[];
+export type ListPart =
+  | {
+      title: string;
+      textEditor?:
+        | {
+            [k: string]: unknown;
+          }[]
+        | null;
+      id?: string | null;
+    }[]
+  | null;
 
 export interface Config {
   collections: {
@@ -24,243 +28,262 @@ export interface Config {
     'payload-migrations': PayloadMigration;
   };
   globals: {
-    site_options: SiteOption;
+    siteOptions: SiteOption;
   };
 }
 export interface Page {
   id: string;
-  title?: string;
-  sections?: (SectionSection | SectionRow | SectionColumns)[];
-  slug?: string;
+  title?: string | null;
+  sections?: (SectionSection | SectionRow | SectionColumns | SectionCarousel)[] | null;
+  slug?: string | null;
   updatedAt: string;
   createdAt: string;
 }
 export interface SectionSection {
-  blocks?: (
-    | BlockHeading
-    | BlockParagraph
-    | BlockAccordion
-    | BlockCard
-    | BlockButtons
-    | BlockTextEditor
-    | BlockDivider
-    | BlockImage
-    | BlockQuote
-    | BlockTabs
-  )[];
+  blocks?:
+    | (
+        | BlockHeading
+        | BlockParagraph
+        | BlockAccordion
+        | BlockCard
+        | BlockButtons
+        | BlockTextEditor
+        | BlockDivider
+        | BlockImage
+        | BlockQuote
+        | BlockTabs
+      )[]
+    | null;
   section_options?: SectionOptions;
-  id?: string;
-  blockName?: string;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'section';
 }
 export interface BlockHeading {
-  size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-  heading?: string;
-  id?: string;
-  blockName?: string;
+  size?: ('h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6') | null;
+  heading?: string | null;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'heading';
 }
 export interface BlockParagraph {
-  paragraph?: string;
-  id?: string;
-  blockName?: string;
+  paragraph?: string | null;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'paragraph';
 }
 export interface BlockAccordion {
-  title?: string;
-  columns?: '1' | '2' | '3' | '4';
+  title?: string | null;
+  columns?: ('1' | '2' | '3' | '4') | null;
   list?: ListPart;
-  id?: string;
-  blockName?: string;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'accordion';
 }
 export interface BlockCard {
-  title?: string;
-  subtitle?: string;
-  image?: string | Media;
-  textEditor?: {
-    [k: string]: unknown;
-  }[];
-  card_color?: string | ThemeColor;
-  id?: string;
-  blockName?: string;
+  title?: string | null;
+  subtitle?: string | null;
+  image?: string | Media | null;
+  textEditor?:
+    | {
+        [k: string]: unknown;
+      }[]
+    | null;
+  card_color?: (string | null) | ThemeColor;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'card';
 }
 export interface Media {
   id: string;
-  alt?: string;
-  title?: string;
+  alt?: string | null;
+  title?: string | null;
   updatedAt: string;
   createdAt: string;
-  url?: string;
-  filename?: string;
-  mimeType?: string;
-  filesize?: number;
-  width?: number;
-  height?: number;
+  url?: string | null;
+  filename?: string | null;
+  mimeType?: string | null;
+  filesize?: number | null;
+  width?: number | null;
+  height?: number | null;
   sizes?: {
     thumbnail?: {
-      url?: string;
-      width?: number;
-      height?: number;
-      mimeType?: string;
-      filesize?: number;
-      filename?: string;
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
     };
     small?: {
-      url?: string;
-      width?: number;
-      height?: number;
-      mimeType?: string;
-      filesize?: number;
-      filename?: string;
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
     };
     medium?: {
-      url?: string;
-      width?: number;
-      height?: number;
-      mimeType?: string;
-      filesize?: number;
-      filename?: string;
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
     };
   };
 }
 export interface ThemeColor {
   id: string;
   name: string;
-  color?: string;
+  color?: string | null;
   updatedAt: string;
   createdAt: string;
 }
 export interface BlockButtons {
-  size?: 'sm' | 'md' | 'lg';
-  justify?: 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-evenly';
+  size?: ('sm' | 'md' | 'lg') | null;
+  justify?: ('flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-evenly') | null;
   buttons: {
     button: ButtonPart;
-    id?: string;
+    id?: string | null;
   }[];
-  id?: string;
-  blockName?: string;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'buttons';
 }
 export interface ButtonPart {
   label: string;
-  link_type?: 'page' | 'url';
-  newTab?: boolean;
-  page_link?: string | Page;
-  url_link?: string;
-  icon?: string;
-  style?: 'primary' | 'secondary';
-  color?: string | ThemeColor;
+  link_type?: ('page' | 'url') | null;
+  newTab?: boolean | null;
+  page_link?: (string | null) | Page;
+  url_link?: string | null;
+  icon?: string | null;
+  style?: ('primary' | 'secondary') | null;
+  color?: (string | null) | ThemeColor;
 }
 export interface BlockTextEditor {
-  textEditor?: {
-    [k: string]: unknown;
-  }[];
-  id?: string;
-  blockName?: string;
+  textEditor?:
+    | {
+        [k: string]: unknown;
+      }[]
+    | null;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'textEditor';
 }
 export interface BlockDivider {
   height: number;
-  color?: string | ThemeColor;
-  id?: string;
-  blockName?: string;
+  color?: (string | null) | ThemeColor;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'divider';
 }
 export interface BlockImage {
   justify: 'left' | 'center' | 'right';
   align: 'flex-start' | 'center' | 'flex-end';
-  fill?: boolean;
+  fill?: boolean | null;
   image: string | Media;
-  id?: string;
-  blockName?: string;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'image';
 }
 export interface BlockQuote {
   text: string;
-  source?: string;
-  color?: string | ThemeColor;
-  id?: string;
-  blockName?: string;
+  source?: string | null;
+  color?: (string | null) | ThemeColor;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'quote';
 }
 export interface BlockTabs {
   list?: ListPart;
-  id?: string;
-  blockName?: string;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'tabs';
 }
 export interface SectionOptions {
-  width?: 'container' | 'container-sm' | 'container-full' | 'container-bleed';
-  padding?: 'top_bottom' | 'top' | 'bottom' | 'none';
-  bg_color?: string | ThemeColor;
-  variant?: string;
-  anchor?: string;
+  width?: ('container' | 'container-sm' | 'container-full' | 'container-bleed') | null;
+  padding?: ('top_bottom' | 'top' | 'bottom' | 'none') | null;
+  bg_color?: (string | null) | ThemeColor;
+  variant?: string | null;
+  anchor?: string | null;
 }
 export interface SectionRow {
-  columns?: '1' | '2';
-  layout?: '6_6' | '5_7' | '7_5' | '4_8' | '8_4' | '3_9' | '9_3';
-  col_1_blocks?: (
-    | BlockHeading
-    | BlockParagraph
-    | BlockAccordion
-    | BlockCard
-    | BlockButtons
-    | BlockTextEditor
-    | BlockDivider
-    | BlockImage
-    | BlockQuote
-  )[];
-  col_2_blocks?: (
-    | BlockHeading
-    | BlockParagraph
-    | BlockAccordion
-    | BlockCard
-    | BlockButtons
-    | BlockTextEditor
-    | BlockDivider
-    | BlockImage
-    | BlockQuote
-  )[];
+  columns?: ('1' | '2') | null;
+  layout?: ('6_6' | '5_7' | '7_5' | '4_8' | '8_4' | '3_9' | '9_3') | null;
+  col_1_blocks?:
+    | (
+        | BlockHeading
+        | BlockParagraph
+        | BlockAccordion
+        | BlockCard
+        | BlockButtons
+        | BlockTextEditor
+        | BlockDivider
+        | BlockImage
+        | BlockQuote
+      )[]
+    | null;
+  col_2_blocks?:
+    | (
+        | BlockHeading
+        | BlockParagraph
+        | BlockAccordion
+        | BlockCard
+        | BlockButtons
+        | BlockTextEditor
+        | BlockDivider
+        | BlockImage
+        | BlockQuote
+      )[]
+    | null;
   section_options?: SectionOptions;
-  id?: string;
-  blockName?: string;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'row';
 }
 export interface SectionColumns {
-  blocks?: (
-    | BlockHeading
-    | BlockParagraph
-    | BlockAccordion
-    | BlockCard
-    | BlockButtons
-    | BlockTextEditor
-    | BlockDivider
-    | BlockImage
-  )[];
+  blocks?:
+    | (
+        | BlockHeading
+        | BlockParagraph
+        | BlockAccordion
+        | BlockCard
+        | BlockButtons
+        | BlockTextEditor
+        | BlockDivider
+        | BlockImage
+      )[]
+    | null;
   column_options?: ColumnOptions;
   section_options?: SectionOptions;
-  id?: string;
-  blockName?: string;
+  id?: string | null;
+  blockName?: string | null;
   blockType: 'columns';
 }
 export interface ColumnOptions {
-  columns?: '2' | '3' | '4' | '5' | '6';
-  justify?: 'flex-start' | 'center' | 'flex-end' | 'stretch';
+  columns?: ('2' | '3' | '4' | '5' | '6') | null;
+  justify?: ('flex-start' | 'center' | 'flex-end' | 'stretch') | null;
+}
+export interface SectionCarousel {
+  blocks?: BlockImage[] | null;
+  section_options?: SectionOptions;
+  id?: string | null;
+  blockName?: string | null;
+  blockType: 'carousel';
 }
 export interface User {
   id: string;
   updatedAt: string;
   createdAt: string;
   email: string;
-  resetPasswordToken?: string;
-  resetPasswordExpiration?: string;
-  salt?: string;
-  hash?: string;
-  loginAttempts?: number;
-  lockUntil?: string;
-  password: string;
+  resetPasswordToken?: string | null;
+  resetPasswordExpiration?: string | null;
+  salt?: string | null;
+  hash?: string | null;
+  loginAttempts?: number | null;
+  lockUntil?: string | null;
+  password: string | null;
 }
 export interface PayloadPreference {
   id: string;
@@ -268,7 +291,7 @@ export interface PayloadPreference {
     relationTo: 'users';
     value: string | User;
   };
-  key?: string;
+  key?: string | null;
   value?:
     | {
         [k: string]: unknown;
@@ -283,8 +306,8 @@ export interface PayloadPreference {
 }
 export interface PayloadMigration {
   id: string;
-  name?: string;
-  batch?: number;
+  name?: string | null;
+  batch?: number | null;
   updatedAt: string;
   createdAt: string;
 }
@@ -294,52 +317,58 @@ export interface SiteOption {
     org_name: string;
     title: string;
     description: string;
-    favicon?: string | Media;
-    meta_image?: string | Media;
-    host_name?: string;
-    ga_id?: string;
-    gtm_id?: string;
+    favicon?: string | Media | null;
+    meta_image?: string | Media | null;
+    host_name?: string | null;
+    ga_id?: string | null;
+    gtm_id?: string | null;
   };
   header?: {
-    logo?: string | Media;
-    nav?: {
-      page?: string | Page;
-      label?: string;
-      has_sublinks?: boolean;
-      sublinks?: {
-        page: string | Page;
-        label?: string;
-        id?: string;
-      }[];
-      id?: string;
-    }[];
+    logo?: string | Media | null;
+    nav?:
+      | {
+          page?: (string | null) | Page;
+          label?: string | null;
+          has_sublinks?: boolean | null;
+          sublinks?:
+            | {
+                page: string | Page;
+                label?: string | null;
+                id?: string | null;
+              }[]
+            | null;
+          id?: string | null;
+        }[]
+      | null;
   };
   footer?: {
-    logo?: string | Media;
-    copywrite_year?: string;
-    colophon?: string;
-    footer_nav?: 'none' | 'main_nav' | 'site_map' | 'custom';
-    footer_links?: {
-      label: string;
-      link_type?: 'page' | 'url';
-      newTab?: boolean;
-      page_link?: string | Page;
-      url_link?: string;
-      id?: string;
-    }[];
+    logo?: string | Media | null;
+    copywrite_year?: string | null;
+    colophon?: string | null;
+    footer_nav?: ('none' | 'main_nav' | 'site_map' | 'custom') | null;
+    footer_links?:
+      | {
+          label: string;
+          link_type?: ('page' | 'url') | null;
+          newTab?: boolean | null;
+          page_link?: (string | null) | Page;
+          url_link?: string | null;
+          id?: string | null;
+        }[]
+      | null;
   };
   contact?: {
-    name?: string;
-    email?: string;
-    phone?: string;
-    address_street?: string;
-    address_street_2?: string;
-    address_city?: string;
-    address_state?: string;
-    zip_code?: string;
+    name?: string | null;
+    email?: string | null;
+    phone?: string | null;
+    address_street?: string | null;
+    address_street_2?: string | null;
+    address_city?: string | null;
+    address_state?: string | null;
+    zip_code?: string | null;
   };
-  updatedAt?: string;
-  createdAt?: string;
+  updatedAt?: string | null;
+  createdAt?: string | null;
 }
 
 
